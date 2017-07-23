6b9fd1d2331c845290d079ae4895cb6d63b56946
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-07-23 15:46:51 -0600

    Tried to add manager that signals clients to stop

--- examples/filter.py
+++ examples/filter.py
@@ -6,7 +6,7 @@ import json
 import zmq
 
 
-def filter(prev, measured):
+def bayes_filter(prev, measured):
     ''' Super lame Bayes filter'''
     return (prev + measured)/2
 
@@ -17,18 +17,30 @@ def main():
     subscriber = context.socket(zmq.SUB)
     subscriber.connect("tcp://localhost:5000")
     subscriber.setsockopt(zmq.SUBSCRIBE, b"sensor")
+
+    kill_sig = context.socket(zmq.SUB)
+    kill_sig.connect("tcp://localhost:9000")
+    kill_sig.setsockopt(zmq.SUBSCRIBE, b"EXIT")
     # }}}
 
     prev = 1.5
 
     try:
         while True:
+            # {{{ wrap this stuff
+            try:  # Replace with polling?
+                kill_sig.recv(flags=zmq.NOBLOCK)
+                print("Got exit command")
+                return
+            except zmq.ZMQError:
+                pass
+            # }}}
             # {{{ combine these lines into one function
             [key, value] = subscriber.recv_multipart()
             value = json.loads(value.decode('utf8'))
+            prev = bayes_filter(prev, value)
             # }}}
-            print("Raw: {}: Filtered: {}".format(value, filter(prev, value)))
-            prev = value
+            print("Raw: {}: Filtered: {}".format(value,prev))
     except KeyboardInterrupt:
         print("Shutting down")
 
--- examples/sensor.py
+++ examples/sensor.py
@@ -14,10 +14,20 @@ def main():
 
     publisher = context.socket(zmq.PUB)
     publisher.bind("tcp://*:5000")
+
+    kill_sig = context.socket(zmq.SUB)
+    kill_sig.connect("tcp://localhost:9000")
+    kill_sig.setsockopt(zmq.SUBSCRIBE, b"EXIT")
     # }}}
 
     try:
         while True:
+            try:  # Replace with polling?
+                kill_sig.recv(flags=zmq.NOBLOCK)
+                print("Got exit command")
+                return
+            except zmq.ZMQError:
+                pass
             # Make this line way simpler.
             publisher.send_multipart(["sensor".encode(),
                                  json.dumps(randrange(133, 255)/100).encode()])
