e74f6e555a9116470743a8e9f711774c8fdc872b
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-08-29 15:32:03 -0600

    Added install requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 setup(name='robocluster',
-      version='0.01',
+      version='0.1',
       description='Distributed robotics framework',
       url='https://github.com/UofSSpaceTeam/robocluster',
       author='UofSSpaceTeam',
       author_email='software@usst.ca',
       license='ECL-2.0',
       packages=['robocluster'],
+      install_requires=[
+          'pyzmq',
+      ],
       zip_safe=False)
 
