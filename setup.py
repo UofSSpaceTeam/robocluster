db71d51b4be7558aa8e21a7b06c28c2e6eff3103
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-08-29 15:20:53 -0600

    Added minimal python package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='robocluster',
+      version='0.01',
+      description='Distributed robotics framework',
+      url='https://github.com/UofSSpaceTeam/robocluster',
+      author='UofSSpaceTeam',
+      author_email='software@usst.ca',
+      license='ECL-2.0',
+      packages=['robocluster'],
+      zip_safe=False)
+
