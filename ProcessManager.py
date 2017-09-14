d777f2926e324ed0d24fd46cb852e7b816cf63c2
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-09-14 12:44:52 -0600

    Fixed some minor styling issues

--- ProcessManager.py
+++ ProcessManager.py
@@ -1,5 +1,3 @@
-# File ProcessManager.py
-#
 # THIS FILE IS SUBJECT TO THE LICENSE TERMS GRANTED BY THE UNIVERSITY OF SASKATCHEWAN SPACE TEAM (USST).
 
 import time
@@ -10,9 +8,7 @@ import shlex
 
 class RoboProcess:
     """
-    Class: RoboProcess
-
-    This class contains the layout of the process
+    This class contains the layout and metadata of a process.
     """
     def __init__(self, cmd):
         """
@@ -32,14 +28,19 @@ class RoboProcess:
 
 class ProcessManager:
     """
-    Class: ProcessManager
-
-    This class represents the processing module for the robocluster operating system.
+    Manages processes that run in the robocluster framework.
+    Processes are just programs that can run independantly
+    from each other, in any language supported by the robocluster library.
+    The ProcessManager is in charge of starting and stopping processes,
+    and monitering their status and handle the event of a crash
+    or a process not responding.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self):
         """Initializes the process manager"""
-        self.process_dict = {} # Process dictionary: stores all processes
+        # process dictionary stores RoboProcess instances indexed
+        # by the process name
+        self.process_dict = {}
 
     def isEmpty(self):
         """
@@ -62,12 +63,12 @@ class ProcessManager:
                 print("Name already taken.")
             else:
                 print("Created {}".format(name))
-                self.process_dict[name] = RoboProcesses(command)
+                self.process_dict[name] = RoboProcess(command)
             return True
         else:
             return False
 
-    def createThread(self):
+    def createThread(self): # TODO: Do we really need this if we have createProcess?
         """
         TODO create a function that takes in parameters and uses those parameters to
         ceate one single thread. The thread must then be stored in "self.T".
@@ -106,29 +107,25 @@ class ProcessManager:
         print ("Shutting down")
 
         for process in self.process_dict:
-            self.process_dict[process].popen.kill()
-            self.process_dict[process].popen.wait()
+            self.stopProcess(process)
 
     def status(self, thread):
         """
         TODO: Return the status of a thread or process
         """
-        if self.isEmpty():
-            return None
-        else:
-            None
+        raise NotImplementedError()
 
     def verify(self, thread):
         """
         TODO: Check the status of the thread or process and perform certain functions based on its status.
         """
-        pass
+        raise NotImplementedError()
 
     def fix(self, thread):
         """
         TODO: Fix a thread or process if its not running properly.
         """
-        pass
+        raise NotImplementedError()
 
 process_names = [["sleep", "python sleeper.py"],
                  ["crash", "python crash.py"]]
@@ -143,9 +140,7 @@ if __name__ == "__main__":
 
     try:
         while True: # Execute the processes.
-            """
-            TODO Run the processes as they should.
-            """
+            # TODO Run the processes as they should.
             for task in taskMgr.process_dict: # Verify tasks.
                 # taskMgr.verify(task.name)
                 pass
