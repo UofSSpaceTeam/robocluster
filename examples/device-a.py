37e233851409313123d38793a5d04f0eb2919138
Jarrod Pas <j.pas@usask.ca>
2017-09-26 05:57:06 -0600

    Fixes redefinition of function

--- examples/device-a.py
+++ examples/device-a.py
@@ -3,11 +3,11 @@ from rcluster import Device
 device = Device('device-a', ('224.0.0.64', 22464))
 
 @device.on('device-b/hello')
-def hello(data):
+def hello_1(data):
     print('first handler for device-b/hello:', data)
 
 @device.on('device-b/hello')
-def hello(data):
+def hello_2(data):
     print('second handler for device-b/hello:', data)
 
 device.run()
