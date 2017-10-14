6c6ae9df3fe21b9b859b32f4871b816d0b273a96
Jarrod Pas <j.pas@usask.ca>
2017-10-14 02:15:28 -0600

    Implement asyncio based device.

--- robocluster/device.py
+++ robocluster/device.py
@@ -1,20 +1,39 @@
 """Message passing for robocluster."""
 
+import asyncio
 from collections import defaultdict
+from contextlib import suppress
+from functools import wraps
 
-from .net import Link, MulticastReceiver
+from .net import Socket
 
 
 class Device:
     """A device to interact with the robocluster network."""
 
-    def __init__(self, name, address):
+    transport = 'json'
+
+    def __init__(self, name, address, loop=None):
+        """Initialize the device."""
         self.name = name
-
-        self.link = Link(*address)
-        self.subscriber = MulticastReceiver(*address)
-
         self.events = defaultdict(list)
+
+        self._loop = loop if loop else asyncio.get_event_loop()
+
+        self._sender = Socket(
+            address,
+            transport=self.transport,
+            loop=self._loop
+        )
+        self._send_queue = asyncio.Queue(loop=self._loop)
+
+        self._receiver = Socket(
+            address,
+            transport=self.transport,
+            multicast=True,
+            loop=self._loop
+        )
+        self._receiver.bind()
 
     def publish(self, topic, data):
         """Publish to topic."""
@@ -22,29 +41,64 @@ class Device:
             'event': '{}/{}'.format(self.name, topic),
             'data': data,
         }
-        self.link.send(packet)
+        return self._send_queue.put(packet)
 
     def on(self, event):
-        """Add a handler to an event."""
-        def adder(handler):
-            """Add a handler to an event."""
-            self.events[event].append(handler)
-            return handler
-        return adder
+        """Add a callback for an event."""
+        def _decorator(callback):
+            self.events[event].append(callback)
+            return callback
+        return _decorator
 
-    def run(self):
-        """Run the device."""
+    def task(self, task):
+        """Create a background task."""
+        self._loop.create_task(task())
+        return task
+
+    def every(self, duration):
+        """Create a background task that runs every duration."""
+        def _decorator(func):
+            @wraps(func)
+            async def _wrapper():
+                while True:
+                    await func()
+                    await self.sleep(duration)
+            self.task(_wrapper)
+            return func
+        return _decorator
+
+    @staticmethod
+    def sleep(seconds):
+        """Sleep the device."""
+        return asyncio.sleep(seconds)
+
+    async def _send_task(self):
+        """Send packets in the send queue."""
+        while True:
+            packet = await self._send_queue.get()
+            await self._sender.send(packet)
+
+    async def _receive_task(self):
+        """Recieve packets and call the appropriate callbacks."""
+        while True:
+            packet, _ = await self._receiver.receive()
+            event, data = packet['event'], packet['data']
+            for callback in self.events[event]:
+                self._loop.create_task(callback(event, data))
+
+    def run_forever(self):
+        """Run device in foreground forever."""
         try:
-            while True:
-                packet, _ = self.subscriber.recieve()
-                if not packet:
-                    continue
-                event = packet['event']
-                if event in self.events:
-                    for handler in self.events[event]:
-                        handler(packet['data'])
+            self._loop.create_task(self._send_task())
+            self._loop.create_task(self._receive_task())
+            self._loop.run_forever()
         except KeyboardInterrupt:
-            pass
-        finally:
-            self.link.close()
-            self.subscriber.close()
+            self.stop()
+
+    def stop(self):
+        """Stop running device."""
+        self._loop.stop()
+        for task in asyncio.Task.all_tasks(loop=self._loop):
+            task.cancel()
+            with suppress(asyncio.CancelledError):
+                self._loop.run_until_complete(task)
--- robocluster/net.py
+++ robocluster/net.py
@@ -1,99 +1,145 @@
-"""Socket wrappers for robocluster."""
-
+import asyncio
 import json
 import socket
 import struct
 from ipaddress import IPv4Address
-from uuid import uuid4
 
 
-def pack(unpacked):
-    """Pack bytes for network."""
-    return json.dumps(unpacked).encode()
+class Socket:
+    """Datagram socket wrapper with encoded transport."""
 
-
-def unpack(packed):
-    """Unpack bytes from network."""
-    return json.loads(packed.decode())
-
-
-class Link:
-    """
-    A bidirectionaly link.
-
-    Used for sending to other Links or MulticastReceivers.
-    """
-
-    def __init__(self, address, port):
-        _ = IPv4Address(address)  # ensure ip address is valid
-        if port < 1024 or port > 65535:
-            raise ValueError('invalid port')
-
-        self.address = (address, port)
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    def send(self, data, to=None):
+    def __init__(self, address, transport='json', multicast=False, loop=None):
         """
-        Send data to an address.
+        Initialize a socket.
 
-        Sends encoded data to the default address (self.address), if to is
-        specified it will send to that address instead.
+        Args:
+            address (str): Address in form 'host:port'
+            transport (str, optional): Transport type to use.
+                Supported values: 'raw', 'utf-8', and 'json'
+                Defaults to 'json'.
+            multicast (bool, optional): If the socket used for multicast.
+                Defaults to False.
+            loop (optional): Event loop to use.
+                Defaults to current event loop.
         """
-        if to is None:
-            to, pid = self.address, None
-        else:
-            to, pid = to
-        return self.socket.sendto(self.encode(data, pid), to)
+        host, port = address.split(':')
+        port = int(port)
+        if port < 1 or port > 65535:
+            raise ValueError('invalid port number')
 
-    def recieve(self):
-        """Recieve data from the network."""
-        raw, source = self.socket.recvfrom(4096)
-        data, pid = self.decode(raw)
-        return data, (source, pid)
+        self._loop = loop if loop else asyncio.get_event_loop()
+
+        self._address = (host, port)
+        self._transport = transport
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._bound = False
+        self._multicast = multicast
+        if self._multicast:
+            ip_address = IPv4Address(host)
+            if not ip_address.is_multicast:
+                raise ValueError('host is not a multicast group')
+
+    @property
+    def buffer_size(self):
+        """Return buffer size of socket."""
+        return self._socket.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
+
+    def bind(self):
+        """Bind to the socket."""
+        if self._multicast:
+            mreq = struct.pack(
+                '4sl',
+                socket.inet_aton(self._address[0]),
+                socket.htonl(socket.INADDR_ANY)
+            )
+            self._socket.setsockopt(
+                socket.IPPROTO_IP,
+                socket.IP_ADD_MEMBERSHIP,
+                mreq
+            )
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.bind(self._address)
+        self._bound = True
+
+    def send(self, data, address=None):
+        """Send a packet to the network."""
+        if self._bound and self._multicast:
+            raise RuntimeError('cannot send on bound multicast socket')
+
+        future = self._loop.create_future()
+        fileno = self._socket.fileno()
+
+        data = self.encode(data)
+        address = address if address else self._address
+
+        def _writer():
+            self._loop.remove_writer(fileno)
+            try:
+                n_bytes = self._socket.sendto(data, address)
+            except (BlockingIOError, InterruptedError):
+                self._loop.add_writer(fileno, _writer)
+            else:
+                future.set_result(n_bytes)
+
+        self._loop.add_writer(fileno, _writer)
+        return future
+
+    def receive(self):
+        """Recieve a packet from the network."""
+        future = self._loop.create_future()
+        fileno = self._socket.fileno()
+
+        def _reader():
+            self._loop.remove_reader(fileno)
+            try:
+                data, address = self._socket.recvfrom(self.buffer_size)
+            except (BlockingIOError, InterruptedError):
+                self._loop.add_reader(fileno, _reader)
+            else:
+                data = self.decode(data)
+                future.set_result((data, address))
+
+        self._loop.add_reader(fileno, _reader)
+        return future
 
     def close(self):
-        """Close the link."""
-        self.socket.close()
+        """Close the socket."""
+        if self._multicast:
+            mreq = struct.pack(
+                '4sl',
+                socket.inet_aton(self._address[0]),
+                socket.htonl(socket.INADDR_ANY)
+            )
+            self._socket.setsockopt(
+                socket.IPPROTO_IP,
+                socket.IP_DROP_MEMBERSHIP,
+                mreq
+            )
 
-    @staticmethod
-    def decode(raw):
-        """Decode raw bytes to data."""
-        packet = unpack(raw)
-        return packet['data'], packet['id']
+        self._socket.close()
 
-    @staticmethod
-    def encode(data, pid=None):
-        """Encode a packet to raw bytes."""
-        packet = {
-            'id': pid if pid else str(uuid4()),
-            'data': data,
-        }
-        return pack(packet)
+    def encode(self, data):
+        """Encode data for network."""
+        if self._transport == 'raw':
+            return data
 
-class MulticastReceiver(Link):
-    """Recieve data from a multicast network."""
+        if self._transport == 'utf-8':
+            return data.encode()
 
-    def __init__(self, multicast_address, port):
-        ip_address = IPv4Address(multicast_address)
-        if not ip_address.is_multicast:
-            raise ValueError('invalid multicast_address')
+        if self._transport == 'json':
+            return json.dumps(data).encode()
 
-        super().__init__(multicast_address, port)
+        raise RuntimeError('transport type not supported')
 
-        sock = self.socket
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        mreq = struct.pack('4sl',
-                           socket.inet_aton(self.address[0]),
-                           socket.htonl(socket.INADDR_ANY))
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-        sock.bind(self.address)
+    def decode(self, data):
+        """Decode data from network."""
+        if self._transport == 'raw':
+            return data
 
-    def close(self):
-        sock = self.socket
-        mreq = struct.pack('4sl',
-                           socket.inet_aton(self.address[0]),
-                           socket.htonl(socket.INADDR_ANY))
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
-        super().close()
+        if self._transport == 'utf-8':
+            return data.decode()
 
-    send = property(doc='Disable send on multicast bound sockets.')
+        if self._transport == 'json':
+            return json.loads(data.decode())
+
+        raise RuntimeError('transport type not supported')
