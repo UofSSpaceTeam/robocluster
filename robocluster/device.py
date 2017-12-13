b2938933d35284af0456e89f078d6421a62b7119
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-12-13 00:06:02 -0600

    Switched to AttributeDict method of storage

--- robocluster/device.py
+++ robocluster/device.py
@@ -11,6 +11,19 @@ from .net import Socket, key_to_multicast
 from .util import duration_to_seconds, as_coroutine
 from .serial import SerialDevice
 
+class AttributeDict(dict):
+    """
+    A dictionary that allows you to acces entries like
+    you would attributes in an object.
+    """
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
 
 class Device:
     """A device to interact with the robocluster network."""
@@ -43,15 +56,12 @@ class Device:
 
         self._serial_devices = {}
 
-        self._storage = {}
+        self.__storage = AttributeDict()
 
-    def __setitem__(self, key, value):
-        """ Allows you to store values with `device['key'] = value` """
-        self._storage[key] = value
-
-    def __getitem__(self, key):
-        """ Allows you to retrieve values with `value = device['key']` """
-        return self._storage[key]
+    @property
+    def storage(self):
+        """Local device storage"""
+        return self.__storage
 
     def publish(self, topic, data):
         """Publish to topic."""
