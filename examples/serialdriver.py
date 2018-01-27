343582337b09e898eef1b7458bb13b6a28003701
Carl Hofmeister <hofmeister.carl@gmail.com>
2018-01-26 20:30:53 -0600

    Update serial device documentation

--- examples/serialdriver.py
+++ examples/serialdriver.py
@@ -3,16 +3,11 @@ from robocluster import SerialDevice, Device
 # import robocluster.util; robocluster.util.DEBUG = True
 
 driver = SerialDevice('/dev/ttyACM0', 'rover')
-
-@driver.every('1s')
-async def print_name():
-    print(driver.name)
-
 tester = Device('tester', 'rover')
 
 @tester.every('1s')
-async def blarg():
-    await tester.publish('blarg', 27)
+async def publish_data():
+    await tester.publish('testSerial', 27)
 
 @tester.on('*/sensor1')
 def print_sensor(event, data):
