3c9026694703d6fcd197b8b251fe6d7fd1a2bb9d
Carl Hofmeister <hofmeister.carl@gmail.com>
2018-04-25 19:42:22 -0600

    Added test for wildcard pub/sub

--- tests/test_device.py
+++ tests/test_device.py
@@ -36,6 +36,35 @@ def test_pubsub():
     device_b.stop()
     assert recieved_message
 
+def test_wildcard_pubsub():
+    # random group so we don't collide in testing
+    group = str(uuid4())
+
+    test_val = random.random()
+    test_key = 'test'
+    recieved_message = False
+    device_a = Device('device-a', group)
+    device_b = Device('device-b', group)
+
+    @device_b.task
+    async def publish():  # pylint: disable=W0612
+        await device_b.publish(test_key, test_val)
+        await device_b.sleep(0.1) # do not hog
+
+    @device_a.on('*/{}'.format(test_key))
+    async def callback(event, data):  # pylint: disable=W0612
+        nonlocal recieved_message
+        assert event == 'device-b/{}'.format(test_key)
+        assert data == test_val
+        recieved_message = True
+
+    device_a.start()
+    device_b.start()
+    sleep(0.5)
+    device_a.stop()
+    device_b.stop()
+    assert recieved_message
+
 def test_every():
     # random group so we don't collide in testing
     group = str(uuid4())
